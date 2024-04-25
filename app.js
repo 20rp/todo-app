889e4a3227f00dfd4f3074f922cee4a453d6e909
Arlo Wotherspoon <awotherspoon@eit.ac.nz>
2024-04-25 19:36:13 +1200

    Added express route

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 9999;
+
+app.get("/", (req, res) => {
+  res.send("Task app - Task Trackr 3000™");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
